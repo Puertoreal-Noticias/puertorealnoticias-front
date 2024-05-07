98379dc34f97b05ca5027443029c290992b3b540
Carlos jesus <carlosjesus.moralespica2000@gmail.com>
2024-05-07 12:23:14 +0200

    Secciones nuevas realizadas y responsive

--- src/app/core/pages/seccion-noticia/seccion-noticia.component.ts
+++ src/app/core/pages/seccion-noticia/seccion-noticia.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { ApiConectService } from '../../services/api-conect.service';
 import { News } from '../../interfaces/news.interface';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-seccion-noticia',
@@ -11,14 +12,24 @@ import { News } from '../../interfaces/news.interface';
 export class SeccionNoticiaComponent implements OnInit {
   public noticias: News[] = [];
   public limit: number = 10;
+  private routeSub!: Subscription;
+
   constructor(
     private route: ActivatedRoute,
-    private ApiConectService: ApiConectService
+    private ApiConectService: ApiConectService,
+    private router: Router
   ) {}
+
   ngOnInit(): void {
-    const categoria = this.route.snapshot.paramMap.get('categoria');
-    console.log(categoria);
-    this.obtenerNoticias(categoria, this.limit);
+    this.routeSub = this.route.params.subscribe((params) => {
+      const categoria = params['categoria'];
+      console.log(categoria);
+      this.obtenerNoticias(categoria, this.limit);
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.routeSub.unsubscribe();
   }
   public obtenerNoticias = (categoria: any, limit: number) => {
     this.ApiConectService.filtrarPorCategoria(categoria, limit).subscribe(
@@ -26,6 +37,7 @@ export class SeccionNoticiaComponent implements OnInit {
         this.noticias = noticias;
         this.noticias.forEach((noticia) => {
           this.ApiConectService.calcularUrl(noticia);
+          console.log(noticias);
         });
       },
       (error) => {
@@ -33,4 +45,15 @@ export class SeccionNoticiaComponent implements OnInit {
       }
     );
   };
+  public formatearFecha = (fecha: Date) => {
+    let fechaPublicacion = new Date(fecha);
+    return fechaPublicacion.toLocaleDateString('es-ES', {
+      year: 'numeric',
+      month: 'long',
+      day: 'numeric',
+    });
+  };
+  public navigateNoticia(id: any) {
+    this.router.navigate(['/noticia-detallada', id]);
+  }
 }
