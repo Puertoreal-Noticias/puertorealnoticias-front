a5308d7251a07998f8d0e16fe3af13b3ab39deb5
Carlos jesus <carlosjesus.moralespica2000@gmail.com>
2024-06-04 12:38:22 +0200

    Cambio en el enviroment

--- src/environments/enviroments.ts
+++ src/environments/enviroments.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  baseUrl: 'http://localhost:3000',
+  baseUrl: 'http://backnoticias-node-production.up.railway.app',
   baseUrlTiempo:
     'https://api.openweathermap.org/data/2.5/weather?lat=36.529121&lon=-6.191900&appid=43d15a9f81ecf57105a1fabca87f9ad1&lang=es',
 };
