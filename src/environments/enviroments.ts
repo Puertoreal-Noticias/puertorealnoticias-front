e40066499b577c0b332541096236caabec9569ba
Puertorealnoticias <jatc_36@hotmail.com>
2024-06-04 19:22:39 +0200

    Update enviroments.ts

--- src/environments/enviroments.ts
+++ src/environments/enviroments.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  baseUrl: 'https://puertorealnoticias-back-production.up.railway.app/',
+  baseUrl: 'https://puertorealnoticias-back-production.up.railway.app',
   baseUrlTiempo:
     'https://api.openweathermap.org/data/2.5/weather?lat=36.529121&lon=-6.191900&appid=43d15a9f81ecf57105a1fabca87f9ad1&lang=es',
 };
