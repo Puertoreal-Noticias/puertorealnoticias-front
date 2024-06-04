3fbfe1c3d4f735f139f93ae6b543fe4155a69d65
Carlos jesus <carlosjesus.moralespica2000@gmail.com>
2024-06-04 20:03:29 +0200

    Enviroment cambiado

--- src/environments/enviroments.ts
+++ src/environments/enviroments.ts
@@ -1,6 +1,6 @@
 export const environment = {
-  // baseUrl: 'https://puertorealnoticias-back-production.up.railway.app',
-  baseUrl: 'http://localhost:3000',
+  baseUrl: 'https://puertorealnoticias-back-production.up.railway.app',
+  // baseUrl: 'http://localhost:3000',
   baseUrlTiempo:
     'https://api.openweathermap.org/data/2.5/weather?lat=36.529121&lon=-6.191900&appid=43d15a9f81ecf57105a1fabca87f9ad1&lang=es',
 };
