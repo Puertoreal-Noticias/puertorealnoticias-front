2c4c1b4afb79f519d90995c95b3ec5a8839fee3b
Carlos jesus <carlosjesus.moralespica2000@gmail.com>
2025-01-11 20:31:03 +0100

    Realizar la llamada para anuncios en HomePageComponent y pasar los datos a AnunciosAmazonComponent

--- src/app/core/components/anuncios-amazon/anuncios-amazon.component.ts
+++ src/app/core/components/anuncios-amazon/anuncios-amazon.component.ts
@@ -1,30 +1,11 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { Ad } from '../../interfaces/ad.interface';
-import { ApiConectService } from '../../services/api-conect.service';
 
 @Component({
   selector: 'app-anuncios-amazon',
   templateUrl: './anuncios-amazon.component.html',
   styleUrls: ['./anuncios-amazon.component.scss'],
 })
-export class AnunciosAmazonComponent implements OnInit {
-  anuncios: Ad[] = [];
-
-  constructor(private apiService: ApiConectService) {}
-
-  ngOnInit(): void {
-    this.obtenerAnuncios(4); // Obteniendo los últimos 4 anuncios
-  }
-
-  obtenerAnuncios(limit: number): void {
-    this.apiService.obtenerAnuncios(limit).subscribe(
-      (ads: Ad[]) => {
-        this.anuncios = ads;
-        console.log('Anuncios recibidos en el componente:', ads);
-      },
-      (error) => {
-        console.error('Error al obtener los anuncios:', error);
-      }
-    );
-  }
+export class AnunciosAmazonComponent {
+  @Input() anuncios: Ad[] = [];
 }
--- src/app/core/pages/home-page/home-page.component.ts
+++ src/app/core/pages/home-page/home-page.component.ts
@@ -1,8 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiConectService } from '../../services/api-conect.service';
+import { Ad } from '../../interfaces/ad.interface';
 
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
   styleUrls: ['./home-page.component.scss'],
 })
-export class HomePageComponent {}
+export class HomePageComponent implements OnInit {
+  anuncios: Ad[] = [];
+
+  constructor(private apiService: ApiConectService) {}
+
+  ngOnInit(): void {
+    this.obtenerAnuncios(4); // Obteniendo los últimos 4 anuncios
+  }
+
+  obtenerAnuncios(limit: number): void {
+    this.apiService.obtenerAnuncios(limit).subscribe(
+      (ads: Ad[]) => {
+        this.anuncios = ads;
+        console.log('Anuncios recibidos en el componente:', ads);
+      },
+      (error) => {
+        console.error('Error al obtener los anuncios:', error);
+      }
+    );
+  }
+}
