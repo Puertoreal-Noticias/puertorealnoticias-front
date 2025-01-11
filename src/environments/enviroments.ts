66b7c33eed9aa4e5df1b96f892ec2581cfabcb58
Carlos jesus <carlosjesus.moralespica2000@gmail.com>
2025-01-11 18:39:43 +0100

    punto

--- src/environments/enviroments.ts
+++ src/environments/enviroments.ts
@@ -1,6 +1,6 @@
 export const environment = {
-  baseUrl: 'http://localhost:3001',
-  // baseUrl: 'https://puertorealnoticias-back-production-5739.up.railway.app',
+  // baseUrl: 'http://localhost:3001',
+  baseUrl: 'https://puertoreal-back-production.up.railway.app',
   baseUrlTiempo:
     'https://api.openweathermap.org/data/2.5/weather?lat=36.529121&lon=-6.191900&appid=43d15a9f81ecf57105a1fabca87f9ad1&lang=es',
 };
