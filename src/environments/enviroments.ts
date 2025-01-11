2f08732a4ba6295e7add8400985e8ba8c28769c9
Carlos jesus <carlosjesus.moralespica2000@gmail.com>
2025-01-11 16:40:51 +0100

    ROLLBACK

--- src/environments/enviroments.ts
+++ src/environments/enviroments.ts
@@ -1,6 +1,6 @@
 export const environment = {
-  // baseUrl: 'http://localhost:3000',
-  baseUrl: 'https://backend-noticiaspuertoreal-production.up.railway.app',
+  baseUrl: 'http://localhost:3001',
+  // baseUrl: 'https://backend-noticiaspuertoreal-production.up.railway.app',
   baseUrlTiempo:
     'https://api.openweathermap.org/data/2.5/weather?lat=36.529121&lon=-6.191900&appid=43d15a9f81ecf57105a1fabca87f9ad1&lang=es',
 };
